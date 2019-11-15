0d47a818d09e49da7a03ad04c6250a6b2b164ee7
Tom Weber <tom.weber@stad.antwerpen.be>
2019-11-15 11:37:49 +0100

    update startup settings

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -105,10 +105,10 @@ namespace DatingApp.API
                     });
                 });
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                // app.UseHsts();
+                app.UseHsts();
             }
 
-            // app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseDefaultFiles();
